f5a42c8392302058757ab07d1e86280e66e5e754
Raaj Raisinghani <raajhriday@gmail.com>
2018-03-13 10:04:09 +0530

    Image Link uploaded into Database

--- app/Http/Controllers/UploadControllers.php
+++ app/Http/Controllers/UploadControllers.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use DB;
 
 class UploadControllers extends Controller
 {
     public function upload(){
 
 		if(Input::hasFile('file')){
-
-			echo 'Uploaded';
 			$file = Input::file('file');
-			$file->move('uploads', $file->getClientOriginalName());
+			$name = $file->getClientOriginalName();
+			DB::insert("insert into xray (image_link,ctr,view) values('uploads/$name',50,'AP')");
+			$file->move('uploads', $name);
+			echo 'Uploaded in the database.';	
 		}
 
 	}
