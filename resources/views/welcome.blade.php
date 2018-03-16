83df8e97c540faa98c0cea76e036ba156a03e022
Raaj Raisinghani <raajhriday@gmail.com>
2018-03-16 09:04:19 +0530

    upload Multiple Files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,15 +60,13 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				
 				<h1>File Upload</h1>
 				<form action="{{ URL::to('upload') }}" method="post" enctype="multipart/form-data">
 					<label>Select image to upload:</label>
-				    <input type="file" name="file" id="file">
+				    <input type="file" name="file" id="file" multiple="multiple">
 				    <input type="submit" value="Upload" name="submit">
 					<input type="hidden" value="{{ csrf_token() }}" name="_token">
 				</form>
-
 			</div>
 		</div>
 	</body>
