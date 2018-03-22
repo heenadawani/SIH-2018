4d9e093cbc4968272d6a1720de8087e5fba950bb
vijay sahani <sahaniv66@gmail.com>
2018-03-22 15:41:58 +0530

    distance part remaining

--- justTest.py
+++ justTest.py
@@ -1,59 +1,66 @@
-
-
-import cv2
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
-import os
- 
-a = np.array([0,0], dtype='float32')
-def getXY(imgpath):
- 
- try:
-   os.environ["DEBUG"]
-   DEBUG = 1
- 
- print "-------------------------------"
- print "Using OpenCV version : ",cv2.__version__
- print "-------------------------------"
- 
- print "TASK : Mimic getxy() feature of MATLAB"
- print "***************************************"
- 
- except KeyError:
-    #print "For verbose mode, set DEBUG flag"
-    DEBUG = 0
- 
- #define the event
- def getxy(event, x, y, flags, param):
-   global a
-   if event == cv2.EVENT_LBUTTONDOWN :
-      a = np.vstack([a, np.hstack([x,y])])
-      if (DEBUG):
-      print "(row, col) = ", (x,y)
- 
-   #Read the image
-   img = cv2.imread("E:\MATLAB\SIHIImageDataset/00000013_005.png")
-   if (DEBUG):
-      print "Reading the image..."
- 
-   #Set mouse CallBack event
-   cv2.namedWindow('image')
-   cv2.setMouseCallback('image', getxy)
-   if (DEBUG):
-      print "Set MouseCallback functionality..."
- 
-   #show the image
-   print "Click to select a point OR press ANY KEY to continue..."
-   cv2.imshow('image', img)
-   cv2.waitKey(0)
-   cv2.destroyAllWindows()
- 
-   #obtain the matrix of the selected points
-   b = a[1:,:]
-   if (DEBUG):
-      print ""
-      print "The clicked points..."
-      print b, b.dtype, b.shape
- 
-   print "The selected points are returned as a float64 MATRIX..."
-   return b
+import bisect
+import random
+import argparse
+from pylab import plot, ginput, show, axis
+
+im = cv2.imread('D:\SIHIImageDataset/00000096_006.png',0)
+#cv2.imshow('image',im)
+refpt=[]
+refpt1=[]
+#enhancement
+equ = cv2.equalizeHist(im)
+#res = np.hstack((im,equ)) #stacking images side-by-side
+#cv2.imwrite('res.png',res)
+'''cv2.imshow('image',equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+#segmentation 
+'''def sp_noise(image,prob):
+   output = np.zeros(image.shape,np.uint8)
+   thres = 1 - prob 
+   for i in range(image.shape[0]):
+       for j in range(image.shape[1]):
+           rdn = random.random()
+           if rdn < prob:
+               output[i][j] = 0
+           elif rdn > thres:
+               output[i][j] = 255
+           else:
+               output[i][j] = image[i][j]
+   return output
+#image = cv2.imread('image.jpg',0) # Only for grayscale image
+#noise_img = sp_noise(equ,0.05)
+#cv2.imwrite('sp_noise.jpg', noise_img)
+cv2.imshow('image',noise_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+
+ret, imgf = cv2.threshold(equ, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)  #using otsu 
+
+def MouseCallback(event,x,y,flag,param):
+  if event == cv2.EVENT_LBUTTONDOWN:
+    refpt = [(x,y)]
+  if event == cv2.EVENT_RBUTTONDOWN:
+    refpt1=[(x1,y1)]
+  cv2.line(imgf,[(x,x1)],[(y,y1)],(255,0,0),3)
+  #cv2.imshow('image',imgf)
+  #distlung  = np.sqrt((x2-x1)**2 + (y1-y1)**2)
+img = np.zeros((512,512,3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',MouseCallback)
+cv2.imshow('image',imgf)
+cv2.setMouseCallback('image',MouseCallback,2)
+
+plt.subplot(1,2,1), plt.imshow(im,cmap = 'gray')
+plt.title('Original Image')
+plt.subplot(1,2,2), plt.imshow(imgf,cmap = 'gray')
+plt.title('output Image')
+plt.show()
+
+
+
