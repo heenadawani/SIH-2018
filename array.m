49ec693327b6ccdb4b28e8b8c0c17eaec52e67c0
vijay sahani <sahaniv66@gmail.com>
2018-03-24 10:51:18 +0530

    reading of the all images in a folder

---
+++ array.m
@@ -0,0 +1,21 @@
+clear all;
+close all;
+clc;
+
+myFolder = 'D:\532\images';
+if ~isdir(myFolder)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+  uiwait(warndlg(errorMessage));
+  return;
+end
+filePattern = fullfile(myFolder, '*.PNG');
+jpegFiles = dir(filePattern);
+for k = 1:length(jpegFiles)
+  baseFileName = jpegFiles(k).name;
+  fullFileName = fullfile(myFolder, baseFileName);
+  fprintf(1, 'Now reading %s\n', fullFileName);
+  imageArray = imread(fullFileName);
+  imshow(imageArray);  % Display image.
+  drawnow; % Force display to update immediately.
+end
+
