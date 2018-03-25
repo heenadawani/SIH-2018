519b749a6b85606257740ec56f0bd8cc6cc243fb
Unknown <varunrathi1910@gmail.com>
2018-03-25 21:09:03 +0530

    Image upload to a folder
    The code uploads a given image to the folder

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
+
+photos=UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST']='C:\\Users\\Mansi\\Desktop\\UploadedImages'
+configure_uploads(app, photos)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 	#return 'Hello World'
 
+#uploading a file to the folder
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method=='POST' and 'inputFile' in request.files:
+        filename=photos.save(request.files['inputFile'])
+        # alert('Uploaded succesfully')
+        return filename
+    return render_template('report.html')
+
 @app.route('/report')
 def report():
-    return render_template('report.html') 
+    return render_template('report.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)       
+    app.run(debug=True)
