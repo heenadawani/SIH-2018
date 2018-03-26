37526aed98b0db4658568dc51769e3eabbf8a983
vijay sahani <sahaniv66@gmail.com>
2018-03-26 08:51:19 +0530

    Reading Processed image and placed into new folder

---
+++ readProcessedImageFolder.m
@@ -0,0 +1,31 @@
+clear all;
+close all;
+clc;
+
+myFolder = 'D:\cardio_PA';
+if ~isdir(myFolder)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+  uiwait(warndlg(errorMessage));
+  return;
+end
+filePattern = fullfile(myFolder, '*.PNG');
+jpegFiles = dir(filePattern);
+for k = 1:length(jpegFiles)
+  baseFileName = jpegFiles(k).name;
+  fullFileName = fullfile(myFolder, baseFileName);
+  fprintf(1, 'Now reading %s\n', fullFileName);
+  i= imread(fullFileName);
+  l= histeq(i);  %for enhacing the image
+J = imnoise(l,'gaussian')
+K= imgaussfilt(J,0.5);   %for filtering the image, standard deviation is by default is 0.5 in gaussian filter 
+level = graythresh(K);   %segmentationk using otsu algo
+BW = im2bw(K,level);     %binary images 
+%imshow(BW);  % Display image.
+  %imwrite(imageArray);
+  %imwrite(imageArray,fullfile('ResizedAx',H));
+  baseFileName = sprintf('%d.png', k);; % Whatever....
+fullFileName = fullfile('D:\cardio', baseFileName);
+imwrite(BW, fullFileName);
+% Force display to update immediately.
+drawnow;
+end
