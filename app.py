93f6b6cdb43e45c5383d96fbbf3ac0e0c2046a2a
Raaj Raisinghani <raajhriday@gmail.com>
2018-03-26 17:26:37 +0530

    uploading and report generation

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+from deeplearning.predict import deepLearning
+import os
 
 app = Flask(__name__)
 
-photos=UploadSet('photos', IMAGES)
+photos = UploadSet('photos', IMAGES)
 
-app.config['UPLOADED_PHOTOS_DEST']='C:\\Users\\Mansi\\Desktop\\UploadedImages'
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
 configure_uploads(app, photos)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-	#return 'Hello World'
 
 #uploading a file to the folder
-@app.route('/upload',methods=['GET','POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method=='POST' and 'inputFile' in request.files:
-        filename=photos.save(request.files['inputFile'])
-        # alert('Uploaded succesfully')
-        return filename
-    return render_template('report.html')
-
-@app.route('/report')
-def report():
-    return render_template('report.html')
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        result=deepLearning()
+        os.system('cls')
+    return render_template('report.html',result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ deeplearning/predict.py
@@ -0,0 +1,109 @@
+def deepLearning():
+
+	import os as os
+	import cv2
+	import numpy as np
+
+
+	img_size =50
+	lr = 1e-3
+	epoch=100
+	step=500
+
+	img_test_dir='C:\\xampp\\htdocs\\smartindiahackathon\\static\\uploads'
+
+	model_name="C:\\xampp\\htdocs\\smartindiahackathon\\static\\deeplearning\\SIH-PA-n=2c-0.001-50-10-500-8conv-basic.model"
+
+	import tflearn
+	from tflearn.layers.conv import conv_2d, max_pool_2d
+	from tflearn.layers.core import input_data, dropout, fully_connected
+	from tflearn.layers.estimator import regression
+
+	import tensorflow as tf
+	tf.reset_default_graph()
+
+	convnet = input_data(shape=[None, img_size, img_size, 1], name='input')
+
+	convnet = conv_2d(convnet, 32, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+	convnet = conv_2d(convnet, 64, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+
+	convnet = conv_2d(convnet, 32, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+	convnet = conv_2d(convnet, 64, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+
+	convnet = conv_2d(convnet, 32, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+	
+	convnet = conv_2d(convnet, 64, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+
+	convnet = conv_2d(convnet, 32, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+	convnet = conv_2d(convnet, 64, 2, activation='relu')
+	convnet = max_pool_2d(convnet, 2)
+
+
+	convnet = fully_connected(convnet, 1024, activation='relu')
+	convnet = dropout(convnet, 0.8)
+
+	
+	convnet = fully_connected(convnet, 2, activation='softmax')
+	convnet = regression(convnet, optimizer='adam', learning_rate=lr, loss='categorical_crossentropy', name='targets')
+
+	model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+	model.load(model_name)
+
+	def label_img(img):
+	    #word_label=img.split('.')[-3]
+	    if img=='Cardiomegaly' : return [1,0]
+	    elif img=='No Cardiomegaly' : return [0,1]
+	    
+	    
+	def create_img_test_data(): 
+	    img_testing_data=[]
+	    #count=0
+	    for img in os.listdir(img_test_dir):
+
+	        path=os.path.join(img_test_dir,img)
+	        img=cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(img_size,img_size))
+	        img_testing_data.append([np.array(img)])
+
+	    np.save('new_img_test_data_trainDown.npy',img_testing_data)
+	    return img_testing_data
+
+	create_img_test_data()
+
+	test_img=np.load('new_img_test_data_trainDown.npy')
+
+	import matplotlib.pyplot as plt
+
+	plt.rcParams['figure.figsize'] = (15,15)
+
+	for num,data in enumerate(test_img[:]):
+
+	    count=0
+	    #img_num = data[1]
+	    img_data = data[0]
+
+	    orig = img_data
+	    data = img_data.reshape(img_size,img_size,1)
+
+	    model_out = model.predict([data])[0]
+	    
+	    if np.argmax(model_out) == 1: str_label='No Cardiomegaly'
+	    else: str_label='Cardiomegaly'
+	        
+	    return str_label
+	        
