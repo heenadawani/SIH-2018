5ae94995d954771aff13b3953c1601add377fadb
Om Uparkar <omauparkar1997@gmail.com>
2018-03-29 21:06:47 +0530

    File upload,loading,report proper final

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template, request
-from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
-from deeplearning.predict import deepLearning
 import os
+from flask import Flask, flash, redirect, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+#from deeplearning.predict import deepLearning
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
-photos = UploadSet('photos', IMAGES)
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'bmp'])
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
-configure_uploads(app, photos)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	 return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#uploading a file to the folder
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        result=deepLearning()
-        os.system('cls')
-    return render_template('report.html',result=result)
+@app.route('/report')
+def report():
+    return render_template('report.html',result="Hey i am here.")
+	
+@app.route('/uploader', methods = ['POST'])
+def uploader():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+
+	for f in request.files.getlist('file'):
+		filename = secure_filename(f.filename)
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	return 'Image Upload Successful.'	
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.secret_key="sih2k18"
+	app.run(debug = True)
