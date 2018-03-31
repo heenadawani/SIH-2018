f19fef9e2839dd335bcf65179808c1e5ddd480b6
vijay sahani <sahaniv66@gmail.com>
2018-03-31 11:43:30 +0530

    final code for the matlab

--- finalcode.m
+++ finalcode.m
@@ -1,41 +1,26 @@
 clc;
 clear all;
 close all;
-i=imread('E:\Downloads\images\images\00000373_000.png');
-K = imadjust(i,[0.4 0.6],[]);
-%T=adaptthresh(i,0.700);
-%Ibw=imbinarize(i,T);
-%figure
-%imshowpair(i,Ibw,'montage')
-threshold =128;
-Ibw = K>threshold;  
-subplot(1,2,1)
-subimage(i);title('input image');
-subplot(1,2,2)
-subimage(Ibw);title('output image');
-%d=imdistline);
 
+i= imread('0.png');
+figure,imshow(i);
 %%
-[x1,y1] = ginput(1);
-[x2,y2] = ginput(1);
-line([x1,x2],[y1,y1],'LineWidth',3);
-distlung  = sqrt((x2-x1)^2 + (y1-y1)^2);
+%figure,imhist(i);
+  
+l= histeq(i);      %for enhancing the image(enhancement)
+figure,imshow(l);
+%%
 
-[x3,y3] = ginput(1);
-[x4,y4] = ginput(1);
-line([x3,x4],[y3,y3],'color','r','LineWidth',3);
-distheart  = sqrt((x4-x3)^2 + (y3-y3)^2);
+K= imgaussfilt(l,0.5);   %for filtering the image,digitized image (filtering) standard deviation is by default is 0.5 in gaussian filter 
+figure,imshow(K);
+%%
+level = graythresh(K);   %(thresholding) using otsu algo
+BW = im2bw(K,level);     %binary images 
+figure,imshow(BW);
+%%
+subplot(1,2,1)
+subimage(i);title('Input Image');
 
-ctr=distheart/distlung;
+subplot(1,2,2)
+subimage(BW);title('Output Image');
 
-if ctr > 1
-    ctr=1/ctr;
-end
-
-if ctr < 0.50
-    fprintf('No Cardiomegaly');
-else
-    fprintf('Cardiomegaly');
-end
-
-ctr
